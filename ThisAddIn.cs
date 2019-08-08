47bb35dde6748f81af1a5bfd66e40cd1bb1f1824
Kirill Sokolov <zzzzz0908@yandex.ua>
2019-08-08 15:39:10 +0300

    Создание ценников (без названия)

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using System.Threading;
 using Excel = Microsoft.Office.Interop.Excel;
 //using Office = Microsoft.Office.Core;
 //using Microsoft.Office.Tools.Excel;
@@ -279,7 +280,11 @@ namespace ExcelAddIn1
 
             Excel.Worksheet newWorksheet;
             newWorksheet = (Excel.Worksheet)this.Application.Worksheets.Add();
+            //Globals.ThisAddIn.Application.ActiveWindow.View = Excel.XlWindowView.xlPageLayoutView;
 
+
+            
+            //Thread.Sleep(300);
             // установить ширину столбиков на новом листе
             char[] cols = { 'A', 'E', 'I' };
 
@@ -293,38 +298,132 @@ namespace ExcelAddIn1
                 cell.Offset[0, 3].ColumnWidth = 2.57;
             }
 
-            CreatePriceTag(newWorksheet.Range["A1"], new string[3]);
-            
+            //CreatePriceTag(newWorksheet.Range["A1"], new string[3]);
+
             // цикл по первому листу
 
             // for ()
             // перевести число в индекс ячейки
             // получить ячейку с листа
             // создать ценник
+             
+            var range = firstSheet.Range["A1:A100"];
 
-            //Globals.ThisAddIn.Application.ActiveWindow.View = Excel.XlWindowView.xlPageLayoutView;
+            foreach (Excel.Range cell in range.Cells)
+            {
+                string value = Convert.ToString(cell.Value2);
 
+                if (value != null)
+                {
+                    int idx = cell.Row;
+                    var targetCell = newWorksheet.Range[GetStartCell(idx)];
 
+                    if (idx % 3 == 1)
+                    {
+                        // 43.50 18.00 28.50 24.00 высота
+                        targetCell.RowHeight = 43.50;
+                        targetCell.Offset[1, 0].RowHeight = 18.00;
+                        targetCell.Offset[2, 0].RowHeight = 28.50;
+                        targetCell.Offset[3, 0].RowHeight = 24.00;
+                    }
+                    // массив данных для ценника: 0 - название, 1 - артикул, 2 - цена опт, 3 - цена розница
+                    string[] info = new string[4];
+                    info[0] = Convert.ToString(cell.Offset[0, 3].Value2);
+                    info[1] = Convert.ToString(cell.Offset[0, 2].Value2);
+                    info[2] = Convert.ToString(cell.Offset[0, 5].Value2);
+                    info[3] = Convert.ToString(cell.Offset[0, 6].Value2); 
 
+                    CreatePriceTag(targetCell, info);
+                }
+                else
+                {
+                    break;
+                }
+            }
 
+            // настроить поля
+            newWorksheet.PageSetup.HeaderMargin = 0;
+            newWorksheet.PageSetup.FooterMargin = 0;
+            double margin = newWorksheet.Application.CentimetersToPoints(0.5);
+            newWorksheet.PageSetup.TopMargin = margin;
+            newWorksheet.PageSetup.BottomMargin = margin;
+            newWorksheet.PageSetup.LeftMargin = margin;
+            newWorksheet.PageSetup.RightMargin = margin;
 
         }
 
-
+        /// <summary>
+        /// Creates price tag
+        /// </summary>
+        /// <param name="firstCell"> Upper left cell of price tag</param>
+        /// <param name="info"></param>
         private void CreatePriceTag(Excel.Range firstCell, string[] info)
         {
-            var workSheet = firstCell.Worksheet;            
+            var workSheet = firstCell.Worksheet;
 
+            // граница всего ценника
+            var range = workSheet.Range[firstCell, firstCell.Offset[3, 3]];
+            range.BorderAround(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+            // общее выравнивание
+            range.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+
+            // линия между ценами
+            range = workSheet.Range[firstCell.Offset[1, 1], firstCell.Offset[2, 1]];
+            range.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle = Excel.XlLineStyle.xlContinuous;
+            range.Borders[Excel.XlBordersIndex.xlEdgeRight].Weight = Excel.XlBorderWeight.xlThick;
+
+
+            // объединение ячеек
             workSheet.Range[firstCell.Offset[1, 0], firstCell.Offset[1, 1]].Merge();
             workSheet.Range[firstCell.Offset[1, 2], firstCell.Offset[1, 3]].Merge();
 
             workSheet.Range[firstCell.Offset[3, 0], firstCell.Offset[3, 1]].Merge();
             workSheet.Range[firstCell.Offset[3, 2], firstCell.Offset[3, 3]].Merge();
 
+            // строка 2
+            var curCell = firstCell.Offset[1, 0];
+            curCell.Value2 = "Цена опт";
+            curCell.Font.Size = 18;
+            curCell.Font.Bold = true;
+
+            curCell = firstCell.Offset[1, 2];
+            curCell.Value2 = "Цена розн.";
+            curCell.Font.Size = 14;
+
+            // значок рубля
+            firstCell.Offset[2, 1].Value2 = "₽";
+            firstCell.Offset[2, 1].Font.Size = 20;
+
+            firstCell.Offset[2, 3].Value2 = "₽";
+            firstCell.Offset[2, 3].Font.Size = 20;
+
+            // артикул
+            curCell = firstCell.Offset[3, 0];
+            curCell.Value2 = info[1];
+            curCell.Font.Size = 16;
+            curCell.HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;
+
+            // флп
+            firstCell.Offset[3, 2].Value2 = "ФЛП Никулин Ю.В.";
+            firstCell.Offset[3, 2].Font.Size = 8;
+
+            // цена опт
+            curCell = firstCell.Offset[2, 0];
+            curCell.Value2 = double.Parse(info[2]);
+            curCell.NumberFormat = "0.00";
+            curCell.Font.Size = 24;
+            curCell.Font.Bold = true;
+
+            // цена розница
+            curCell = firstCell.Offset[2, 2];
+            curCell.Value2 = double.Parse(info[3]);
+            curCell.NumberFormat = "0.00";
+            curCell.Font.Size = 18;
+
+            // объединять первую строчку в конце
             workSheet.Range[firstCell, firstCell.Offset[0, 3]].Merge();
 
-
-
+            // название товара
         }
 
         private string GetStartCell(int idx)
